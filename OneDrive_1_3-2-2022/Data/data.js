849a21822d4783f06d8eae8b4e6d8269e86467e9
picci02 <103245935+picci02@users.noreply.github.com>
2022-04-08 10:18:42 +0200

    Add files via upload

---
+++ OneDrive_1_3-2-2022/Data/data.js
@@ -0,0 +1,19 @@
+
+//this js file is used as a database for information //
+
+let data = 
+[
+    {
+       "name" : "Joe", //use comma to add more porperties //
+       "age" :" 60" ,
+       "username" : "jb" ,
+       "password" : "abc",      
+    }, //use comma at the end of the curly brackets of an object //
+
+    {           
+       "name" : "Mark", 
+       "age" : "50",
+       "username" : "mf" ,
+       "password" : "def",      
+    }
+]
